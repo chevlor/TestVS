441067068beb9a3b6185810856d4c6913ff3687c
chevlor <lorin.chevre@bluewin.ch>
2020-02-06 16:45:37 +0100

    En cours.

--- TestLorin/Program.cs
+++ TestLorin/Program.cs
@@ -10,7 +10,7 @@ namespace TestLorin
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Coucou");
+            
         }
     }
 }
---
+++ TestLorin/Thermometre.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestLorin
+{
+    class Thermometre
+    {
+        private int temperature;
+        private char choix;
+        private string choixAffichage;
+
+        public Thermometre(int temperature)
+        {
+            this.temperature = temperature;
+        }
+
+        public void Choix(char choix)
+        {
+            this.choix = choix;
+
+            switch (choix)
+            {
+                case 'C':
+                    choixAffichage = "degrés celius";
+                    break;
+
+                case 'F':
+                    choixAffichage = "fahrenheit";
+                    break;
+            }
+        }
+
+        public override string ToString()
+        {
+            if(choix == 'F')
+            {
+                temperature = (temperature * (9 / 5)) + 32;
+            }
+
+            return $"La température est de {temperature} {choixAffichage}.";
+
+        }
+    }
+}
